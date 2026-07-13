d360e4c096d56c78d6e3eeb544f761a20545457b
darkwaffle <darkewaffle@gmail.com>
2026-07-13 13:53:09 -0400

    Removed unneeded reference to NPCIndex.

--- chunks/chunk_NPCSpawn.lua
+++ chunks/chunk_NPCSpawn.lua
@@ -2,7 +2,6 @@ function ParseNPCSpawn(id, original, modified, injected, blocked)
 	local NPCSpawnPacket = WINDOWER_PACKETS.parse('incoming', original)
 
 	local NPCID = NPCSpawnPacket["ID"]
-	local NPCIndex = NPCSpawnPacket["Index"]
 	local NPCType = NPCSpawnPacket["Type"]
 
 	if NPCType == 3 then
